a2b03ef0094aed21eed4ef0e8b4a7bbef5d1dbd9
kazusa1412 <549324884@qq.com>
2021-02-09 00:56:58 +0800

    server.go

--- src/server.go
+++ src/server.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
 type Server struct {
 	Ip string
 	Port int
@@ -13,6 +19,31 @@ func NewServer(ip string,port int) *Server {
 	}
 }
 
+func (this *Server) Handler(connection net.Conn){
+	fmt.Println("connect success")
+}
+
+// 启动服务
 func (this *Server) Start() {
+	// socket listen
+	var listener,err = net.Listen("tcp",this.Ip + ":" + strconv.Itoa(this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err: ",err)
+		return
+	}
+
+	// finally close listen socket
+	defer listener.Close()
+
+	// accept
+	for {
+		var connection,err = listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err: ",err)
+			continue
+		}
+		// do handler
+		go this.Handler(connection)
+	}
 
 }
